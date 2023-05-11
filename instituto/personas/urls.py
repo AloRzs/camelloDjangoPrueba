b56e780269aa6128e55431a7069814f26dd91454
AloRzs <124218558+AloRzs@users.noreply.github.com>
2023-05-11 19:45:14 -0400

    Se realizaron cambios a urls (no todos )

--- instituto/personas/urls.py
+++ instituto/personas/urls.py
@@ -10,4 +10,7 @@ from . import views
 urlpatterns= [
     path('index',views.index,name='index'),
     path('lista_productos',views.lista_productos, name='lista_productos'),
+    path('contacto',views.contacto, name='contacto'),
+    path('nosotros',views.nosotros, name='nosotros'),
+    path('ers',views.ers, name='ers'),
 ]
